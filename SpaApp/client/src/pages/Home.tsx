07e707426b347d73f46c6794be10cb3cd6587268
dedserch <dedmemchik@gmail.com>
2024-11-02 03:21:18 +0300

    update home page

--- SpaApp/client/src/pages/Home.tsx
+++ SpaApp/client/src/pages/Home.tsx
@@ -1,5 +1,20 @@
 import React from "react"
 
 export const Home: React.FC = () => {
-  return <h1>Hello World</h1>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
+      <div className="text-center px-6 md:px-12">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">API & SPA</h1>
+        <h2 className="text-lg md:text-2xl font-medium mb-6">
+          Educational project developed for learning React Router DOM and Axios.
+        </h2>
+        <p className="text-md md:text-lg max-w-2xl mx-auto">
+          This website serves as an assignment for my Web Programming course at
+          Belarusian State University. It demonstrates the use of React, React
+          Router, Axios, and the {`{JSON}`} Placeholder API to create a
+          multi-page application.
+        </p>
+      </div>
+    </div>
+  )
 }
