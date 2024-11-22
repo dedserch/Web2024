b3bf3f39a7538617a61ec367dd1cfe4666dcf178
dedserch <dedmemchik@gmail.com>
2024-11-22 19:35:06 +0300

    Add: log out button

--- Notion/src/components/ui/Navigation.tsx
+++ Notion/src/components/ui/Navigation.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import { AuthService } from "../../services/AuthService"
 
 export const Navigation: React.FC = () => {
+  const handleLogout = async (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    await AuthService.logout()
+    window.location.href = "/auth"
+  }
+
   return (
     <nav className="flex space-x-6 text-lg">
       <NavLink
@@ -15,6 +22,13 @@ export const Navigation: React.FC = () => {
         className="hover:text-gray-200 transition-colors duration-200 border-b-2 border-transparent hover:border-gray-200"
       >
         Notes
+      </NavLink>
+      <NavLink
+        to="/auth"
+        onClick={handleLogout}
+        className="hover:text-gray-200 transition-colors duration-200 border-b-2 border-transparent hover:border-gray-200"
+      >
+        Log out
       </NavLink>
     </nav>
   )
